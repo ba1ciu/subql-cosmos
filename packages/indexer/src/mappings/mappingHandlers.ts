0be1ebd2e291d21393b079899db6ee24f7eefcbe
Jakub Kobeldys <jakub@kobeldys.com>
2023-03-23 12:31:25 +0100

    Finishing indexed data model

--- packages/indexer/src/mappings/mappingHandlers.ts
+++ packages/indexer/src/mappings/mappingHandlers.ts
@@ -1,4 +1,4 @@
-import { ExecuteEvent, Message, Transaction, Block, CreditCollection, EventData, MaterialData, MetadataURI, MediaFile, BinaryFile, ApplicantData, WebReference } from "../types";
+import { ExecuteEvent, Message, Transaction, Block, CreditCollection, EventData, MaterialData, MetadataUri, MediaFile, BinaryFile, ApplicantData, WebReference, IssuanceInfo } from "../types";
 import {
   CosmosEvent,
   CosmosBlock,
@@ -6,7 +6,6 @@ import {
   CosmosTransaction,
 } from "@subql/types-cosmos";
 import fetch from "node-fetch";
-import { CreditCollectionProps } from "../types/models/CreditCollection";
 
 
 export async function handleBlock(block: CosmosBlock): Promise<void> {
@@ -53,38 +52,28 @@ export async function handleIssueCredits(event: CosmosEvent): Promise<void> {
   let denom = fetchPropertyFromEvent(event, "denom");
   let amount = fetchPropertyFromEvent(event, "amount");
   let projectId = fetchPropertyFromEvent(event, "project_id");
-  let issuanceDate = Date.now();
+  let creditTypeAbbreviation = fetchPropertyFromEvent(event, "credit_class_abbreviation");
 
-  const metadataUris = fetchPropertyFromEvent(event, "metadata_urls");
-  const metadataUrisArray = metadataUris.split(",");
+  const metadataUrls = fetchPropertyFromEvent(event, "metadata_uris");
+  const metadataUrlsArray = decodeUriArrayFromEvent(metadataUrls);
 
-  const res = await fetch("https://bafkreidk2n5ymosfbbvfsv4pmsqiajika7kbzqmtca24bsr34co5ftstde.ipfs.w3s.link");
-  const data = await res.json();
-
-  const eventData = findPropById("event_data", data["credit_props"]);
-  const eventLocation = findPropById("location", eventData.content);
-  const eventAmount = findPropById("amount", eventData.content);
-  const eventMagnitude = findPropById("magnitude", eventData.content);
-  const eventMaterial = findPropById("material", eventData.content);
-  const eventRegistrationDate = findPropById("registration_date", eventData.content);
-  const aggregationLocation = findPropById("aggregation_location", data["credit_props"]);
-  aggregationX = aggregationLocation.content.x;
-  aggregationY = aggregationLocation.content.y;
-
-  const eventData = EventData.create({
-  });
-
-  const eventRecord = CreditCollection.create({
-    id: `${event.tx.hash}-${event.msg.idx}-${event.idx}`,
+  const creditCollection = CreditCollection.create({
+    id: denom,
     denom: denom,
     projectId: parseInt(projectId),
     activeAmount: BigInt(amount),
     retiredAmount: BigInt(0),
-    issuanceDate: issuanceDate.toString(),
-    x_aggregation_location: aggregationX,
-    y_aggregation_location: aggregationY,
+    creditType: creditTypeAbbreviation,
   });
-  await eventRecord.save();
+  await creditCollection.save();
+
+  await handleMetadataUris(metadataUrlsArray, creditCollection.id);
+  
+  for (let [i, metadataUri] of metadataUrlsArray.entries()) {
+    const metadata = await fetchMetadataFromIpfs(metadataUri);
+    await handleIssuanceInfo(metadata, creditCollection.id, i.toString());
+  }
+
 }
 
 function findPropById(id: string, creditProps: any[]): any {
@@ -95,24 +84,54 @@ function fetchPropertyFromEvent(event: CosmosEvent, property: string): string {
   return event.event.attributes.find((attr) => attr.key === property)?.value.replace(/"/g, "");
 }
 
+function decodeUriArrayFromEvent(eventUris: string): string[] {
+  eventUris = eventUris.replace(/\[/g, "");
+  eventUris = eventUris.replace(/\]/g, "");
+  return eventUris.split(",");
+}
+
 async function fetchMetadataFromIpfs(url: string): Promise<any> {
   const reqUri = "https://" + url.substring(7) + ".ipfs.w3s.link";
   const res = await fetch(reqUri);
   return res.json();
 }
 
-async function handleEventData(eventDataJson: any, creditCollectionId: string, eventIndex: string): Promise<void> {
-  const eventData = EventData.create({
-    id: `${creditCollectionId}-${eventIndex}`,
-    latitude: findPropById("location", eventDataJson.content).content.latitude,
-    longitude: findPropById("location", eventDataJson.content).content.longitude,
-    amount: findPropById("amount", eventDataJson.content).content,
-    magnitude: findPropById("magnitude", eventDataJson.content).content,
-    registrationDate: findPropById("registration_date", eventDataJson.content).content,
+async function handleIssuanceInfo(metadata: any, creditCollectionId: string, issuanceInfoIndex: string): Promise<void> {
+  const issuanceInfo = IssuanceInfo.create({
+    id: `${creditCollectionId}-${issuanceInfoIndex}`,
+    issuanceDate: findPropById("issuance_date", metadata["credit_props"]).content,
+    creditType: findPropById("credit_type", metadata["credit_props"]).content,
+    // For now, we take only amount from first event
+    amount: findPropById("amount", findPropById("event_data", metadata["credit_props"]).content[0]).content,
+    aggregationLatitude: findPropById("aggregation_location", metadata["credit_props"]).content.latitude,
+    aggregationLongitude: findPropById("aggregation_location", metadata["credit_props"]).content.longitude,
     creditCollectionId: creditCollectionId,
   })
+  await issuanceInfo.save();
+  const eventData = findPropById("event_data", metadata["credit_props"]);
+  for (let [i, event] of eventData.content.entries()) {
+    await handleEventData(event, issuanceInfo.id, i.toString());
+  }
+  const mediaFiles = findPropById("event_media", metadata["credit_props"]);
+  await handleMediaFiles(mediaFiles, issuanceInfo.id);
+  const binaryFiles = findPropById("event_files", metadata["credit_props"]);
+  await handleBinaryFiles(binaryFiles, issuanceInfo.id);
+  const applicantData = findPropById("applicant_data", metadata["credit_props"]);
+  await handleApplicantData(applicantData, issuanceInfo.id);
+}
+
+async function handleEventData(eventDataJson: any, issuanceInfoId: string, eventIndex: string): Promise<void> {
+  const eventData = EventData.create({
+    id: `${issuanceInfoId}-${eventIndex}`,
+    latitude: findPropById("location", eventDataJson).content.latitude,
+    longitude: findPropById("location", eventDataJson).content.longitude,
+    amount: findPropById("amount", eventDataJson).content,
+    magnitude: findPropById("magnitude", eventDataJson).content,
+    registrationDate: findPropById("registration_date", eventDataJson).content,
+    issuanceInfoId: issuanceInfoId,
+  })
   await eventData.save();
-  for (let [i, material] of findPropById("material", eventDataJson.content).content.entries()) {
+  for (let [i, material] of findPropById("material", eventDataJson).content.entries()) {
     await handleMaterialData(material, eventData.id, i);
   }
 }
@@ -129,7 +148,7 @@ async function handleMaterialData(materialDataJson: any, eventDataId: string, ma
 
 async function handleMetadataUris(metadataUris: string[], creditCollectionId: string): Promise<void> {
   for (let [i, url] of metadataUris.entries()) {
-    const metadataUri = MetadataURI.create({
+    const metadataUri = MetadataUri.create({
       id: `${creditCollectionId}-${i}`,
       url: url,
       creditCollectionId: creditCollectionId,
@@ -138,46 +157,46 @@ async function handleMetadataUris(metadataUris: string[], creditCollectionId: st
   }
 }
 
-async function handleMediaFiles(mediaFiles: any, creditCollectionId: string): Promise<void> {
-  for (let [i, mediaFileJson] of mediaFiles.entries()) {
+async function handleMediaFiles(mediaFiles: any, issuanceInfoId: string): Promise<void> {
+  for (let [i, mediaFileJson] of mediaFiles.content.entries()) {
     const mediaFile = MediaFile.create({
-      id: `${creditCollectionId}-${i}`,
+      id: `${issuanceInfoId}-${i}`,
       url: mediaFileJson.url,
       name: mediaFileJson.name,
-      creditCollectionId: creditCollectionId,
+      issuanceInfoId: issuanceInfoId,
     })
     await mediaFile.save();
   }
 }
 
-async function handleBinaryFiles(binaryFiles: any, creditCollectionId: string): Promise<void> {
-  for (let [i, binaryFileJson] of binaryFiles.entries()) {
+async function handleBinaryFiles(binaryFiles: any, issuanceInfoId: string): Promise<void> {
+  for (let [i, binaryFileJson] of binaryFiles.content.entries()) {
     const binaryFile = BinaryFile.create({
-      id: `${creditCollectionId}-${i}`,
+      id: `${issuanceInfoId}-${i}`,
       url: binaryFileJson.url,
       name: binaryFileJson.name,
-      creditCollectionId: creditCollectionId,
+      issuanceInfoId: issuanceInfoId,
     })
     await binaryFile.save();
   }
 }
 
-async function handleApplicantData(applicantDataJson: any, creditCollectionId: string): Promise<void> {
+async function handleApplicantData(applicantDataJson: any, issuanceInfoId: string): Promise<void> {
   const applicantData = ApplicantData.create({
-    id: creditCollectionId,
-    name: applicantDataJson.name,
-    description: applicantDataJson.description,
-    creditCollectionId: creditCollectionId,
+    id: `${issuanceInfoId}`,
+    name: applicantDataJson.content.name,
+    description: applicantDataJson.content.description,
+    issuanceInfoId: issuanceInfoId,
   })
   await applicantData.save();
-  await handleWebReferences(applicantDataJson.web_refs, applicantData.id);
+  await handleWebReferences(applicantDataJson.content["web_refs"], applicantData.id);
 }
 
 async function handleWebReferences(webReferences: any, applicantId: string): Promise<void> {
-  for (let [i, webReferenceJson] of webReferences.entries()) {
+  for (let [i, url] of webReferences.entries()) {
     const webReference = WebReference.create({
       id: `${applicantId}-${i}`,
-      url: webReferenceJson.url,
+      url: url,
       applicantDataId: applicantId,
     })
     await webReference.save();
