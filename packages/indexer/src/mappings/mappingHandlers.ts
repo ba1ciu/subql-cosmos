1d683f0e4dc62827f823f99a46032f8cc8fd328b
Jakub Kobeldys <jakub@kobeldys.com>
2023-05-29 11:45:06 +0200

    Cache for credit balances and retirements

--- packages/indexer/src/mappings/mappingHandlers.ts
+++ packages/indexer/src/mappings/mappingHandlers.ts
@@ -1,4 +1,4 @@
-import { ExecuteEvent, Message, Transaction, Block, CreditCollection, EventData, MaterialData, MetadataUri, MediaFile, BinaryFile, ApplicantData, WebReference, CreditData, CreateListingWasmEvent, MarketplaceListing, BuyCreditsWasmEvent, UpdateListingWasmEvent, CancelListingWasmEvent, Country } from "../types";
+import { ExecuteEvent, Message, Transaction, Block, CreditCollection, EventData, MaterialData, MetadataUri, MediaFile, BinaryFile, ApplicantData, WebReference, CreditData, CreateListingWasmEvent, MarketplaceListing, BuyCreditsWasmEvent, UpdateListingWasmEvent, CancelListingWasmEvent, Country, Organization, Wallet, CreditBalance, TransferedCreditsEvent, RetiredCreditsEvent, Certificate, CreditOffsetCertificate } from "../types";
 import {
   CosmosEvent,
   CosmosBlock,
@@ -48,6 +48,15 @@ export async function handleEvent(event: CosmosEvent): Promise<void> {
   await eventRecord.save();
 }
 
+async function createNewWallet(address: string): Promise<Wallet> {
+  const wallet = Wallet.create({
+    id: address,
+    address: address,
+  });
+  await wallet.save();
+  return wallet;
+}
+
 export async function handleCreateListing(event: CosmosEvent): Promise<void> {
 
   const listingOwner = fetchPropertyFromEvent(event, "listing_owner");
@@ -55,6 +64,7 @@ export async function handleCreateListing(event: CosmosEvent): Promise<void> {
   const numberOfCredits = BigInt(fetchPropertyFromEvent(event, "number_of_credits"));
   const pricePerCreditAmount = BigInt(fetchPropertyFromEvent(event, "price_per_credit_amount"));
   const pricePerCreditDenom = fetchPropertyFromEvent(event, "price_per_credit_denom");
+
 
   const createListingWasmEvent = CreateListingWasmEvent.create({
     id: `${event.tx.hash}-${event.msg.idx}-${event.idx}`,
@@ -70,6 +80,7 @@ export async function handleCreateListing(event: CosmosEvent): Promise<void> {
     owner: listingOwner,
     denom: denom,
     amount: numberOfCredits,
+    initialAmount: numberOfCredits,
     pricePerCreditAmount: pricePerCreditAmount,
     pricePerCreditDenom: pricePerCreditDenom,
     creditCollectionId: denom,
@@ -97,6 +108,7 @@ export async function handleUpdateListing(event: CosmosEvent): Promise<void> {
 
   const marketplaceListing = await MarketplaceListing.get(`${listingOwner}-${denom}`);
   marketplaceListing.amount = numberOfCredits;
+  marketplaceListing.initialAmount = numberOfCredits;
   marketplaceListing.pricePerCreditAmount = pricePerCreditAmount;
   marketplaceListing.pricePerCreditDenom = pricePerCreditDenom;
   await marketplaceListing.save();
@@ -166,11 +178,126 @@ export async function handleWasmEvents(event: CosmosEvent): Promise<void> {
   }
 }
 
+export async function handleTransferCredits(event: CosmosEvent): Promise<void> {
+  const sender = fetchPropertyFromEvent(event, "sender");
+  const recipient = fetchPropertyFromEvent(event, "recipient");
+  const denom = fetchPropertyFromEvent(event, "denom");
+  const amount = BigInt(fetchPropertyFromEvent(event, "amount"));
+
+  const transferedCreditsEvent = TransferedCreditsEvent.create({
+    id: `${event.tx.hash}-${event.msg.idx}-${event.idx}`,
+    sender: sender,
+    recipient: recipient,
+    amount: amount,
+    creditCollectionId: denom,
+  });
+  await transferedCreditsEvent.save();
+
+  let senderBalance = await CreditBalance.get(`${sender}-${denom}`);
+  senderBalance.amountActive = senderBalance.amountActive - amount;
+  await senderBalance.save();
+
+  let recipientWallet = await Wallet.get(recipient);
+  if (!recipientWallet) {
+    recipientWallet = Wallet.create({
+      id: recipient,
+      address: recipient,
+    });
+    await recipientWallet.save();
+  }
+  let recipientBalance = await CreditBalance.get(`${recipient}-${denom}`);
+  if (!recipientBalance) {
+    recipientBalance = CreditBalance.create({
+      id: `${recipient}-${denom}`,
+      walletId: recipientWallet.id,
+      creditCollectionId: denom,
+      amountActive: amount,
+      amountRetired: BigInt(0),
+    });
+    await recipientBalance.save();
+  } else {
+    recipientBalance.amountActive = recipientBalance.amountActive + amount;
+    await recipientBalance.save();
+  }
+}
+
+export async function handleRetiredCredits(event: CosmosEvent): Promise<void> {
+  const owner = fetchPropertyFromEvent(event, "owner");
+  const denom = fetchPropertyFromEvent(event, "denom");
+  const amount = BigInt(fetchPropertyFromEvent(event, "amount"));
+
+  const retiredCreditsEvent = RetiredCreditsEvent.create({
+    id: `${event.tx.hash}-${event.msg.idx}-${event.idx}`,
+    owner: owner,
+    amount: amount,
+    creditCollectionId: denom,
+  });
+  await retiredCreditsEvent.save();
+
+  let ownerBalance = await CreditBalance.get(`${owner}-${denom}`);
+  ownerBalance.amountActive = ownerBalance.amountActive - amount;
+  ownerBalance.amountRetired = ownerBalance.amountRetired + amount;
+  await ownerBalance.save();
+
+  let creditCollection = await CreditCollection.get(denom);
+  creditCollection.activeAmount = creditCollection.activeAmount - amount;
+  creditCollection.retiredAmount = creditCollection.retiredAmount + amount;
+  await creditCollection.save();
+}
+
+function findCertificateDataValueByKey(certificate: any[], key: string): string {
+  return certificate.find(data => data.key === key).value;
+}
+
+async function handleOffsetCertificate(certificateId: string, owner: string, certificateData: string): Promise<void> {
+  certificateData = certificateData.replace(/(?:\\\\)*\\(?!\\)/g, '');
+  const certificate = JSON.parse(certificateData);
+  const denom = findCertificateDataValueByKey(certificate, "denom");
+  const amount = BigInt(findCertificateDataValueByKey(certificate, "amount"));
+  const retiringEntityAddress = findCertificateDataValueByKey(certificate, "retiring_entity_address");
+  const retiringEntityName = findCertificateDataValueByKey(certificate, "retiring_entity_name");
+  const retiringEntityAdditionalData = findCertificateDataValueByKey(certificate, "retiring_entity_additional_data");
+
+  const offsetCertificate = CreditOffsetCertificate.create({
+    id: certificateId,
+    denom: denom,
+    amount: amount,
+    retiringEntityAddress: retiringEntityAddress,
+    retiringEntityName: retiringEntityName,
+    retiringEntityAdditionalData: retiringEntityAdditionalData,
+    walletId: owner,
+  });
+  await offsetCertificate.save();
+}
+
+export async function handleCreateCertificate(event: CosmosEvent): Promise<void> {
+  const certificateId = fetchPropertyFromEvent(event, "certificate_id");
+  const issuer = fetchPropertyFromEvent(event, "issuer");
+  const owner = fetchPropertyFromEvent(event, "owner");
+  const certificateType = fetchPropertyFromEvent(event, "certificate_type");
+  const additionalData = fetchPropertyFromEvent(event, "additional_data");
+
+  const certificate = Certificate.create({
+    id: certificateId,
+    type: certificateType,
+    issuer: issuer,
+    owner: owner,
+    data: additionalData,
+    walletId: owner,
+  });
+  await certificate.save();
+
+  if (certificateType === "CREDIT_RETIREMENT") {
+    await handleOffsetCertificate(certificateId, owner, additionalData);
+  }
+}
+
 export async function handleIssueCredits(event: CosmosEvent): Promise<void> {
   let denom = fetchPropertyFromEvent(event, "denom");
   let amount = fetchPropertyFromEvent(event, "amount");
   let projectId = fetchPropertyFromEvent(event, "project_id");
   let applicantId = fetchPropertyFromEvent(event, "applicant_id");
+  let recipient = fetchPropertyFromEvent(event, "recipient");
   let creditTypeAbbreviation = fetchPropertyFromEvent(event, "credit_type_abbreviation");
 
   const metadataUrls = fetchPropertyFromEvent(event, "metadata_uris");
@@ -187,6 +314,19 @@ export async function handleIssueCredits(event: CosmosEvent): Promise<void> {
   });
   await creditCollection.save();
 
+  let wallet = await Wallet.get(recipient);
+  if (!wallet) {
+    wallet = await createNewWallet(recipient);
+  }
+  const creditBalance = CreditBalance.create({
+    id: `${recipient}-${denom}`,
+    creditCollectionId: denom,
+    amountActive: BigInt(amount),
+    amountRetired: BigInt(0),
+    walletId: wallet.id,
+  });
+  await creditBalance.save();
+
   await handleMetadataUris(metadataUrlsArray, creditCollection.id);
 
   for (let [i, metadataUri] of metadataUrlsArray.entries()) {
@@ -200,13 +340,23 @@ function findPropById(id: string, creditProps: any[]): any {
   return creditProps.find((prop) => prop.id === id);
 }
 
+function removeDoubleQuotes(str: string): string {
+  // if value starts and ends with double quotes, remove them
+  if (str.startsWith("\"") && str.endsWith("\"")) {
+    str = str.substring(1, str.length - 1);
+  }
+  return str;
+}
+
 function fetchPropertyFromEvent(event: CosmosEvent, property: string): string {
-  return event.event.attributes.find((attr) => attr.key === property)?.value.replace(/"/g, "");
+  const prop = event.event.attributes.find((attr) => attr.key === property)?.value;
+  return removeDoubleQuotes(prop);
 }
 
 function decodeUriArrayFromEvent(eventUris: string): string[] {
   eventUris = eventUris.replace(/\[/g, "");
   eventUris = eventUris.replace(/\]/g, "");
+  eventUris = eventUris.replace(/\"/g, "");
   return eventUris.split(",");
 }
 
@@ -248,7 +398,7 @@ async function handleEventData(eventDataJson: any, creditDataId: string, eventIn
   let country = "";
 
   try {
-    const reqUri = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&key=$API_KEY";
+    const reqUri = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&key=$KEY";
     const response = await fetch(reqUri);
     const result = await response.json();
 
@@ -337,6 +487,10 @@ async function handleApplicantData(applicantDataJson: any, creditDataId: string)
     creditDataId: creditDataId,
   })
   await applicantData.save();
+  const organization = Organization.create({
+    id: applicantDataJson.content.name,
+  });
+  await organization.save();
   await handleWebReferences(applicantDataJson.content["web_refs"], applicantData.id);
 }
 
